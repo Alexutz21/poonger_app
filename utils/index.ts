2447913e06fe46d9150dc9edcf5e3deff2b735d1
Alexandru Constantin Lungu <alex.lungu21@yahoo.com>
2022-07-29 21:30:09 +0300

    minor changes to home screen

--- utils/index.ts
+++ utils/index.ts
@@ -1,8 +1,20 @@
+import type { NextPage } from 'next';
 import axios from 'axios';
+import Home from '../pages';
+
 
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const createOrGetUser = async (response:
   any, addUser: any) => {
   
-  };
+};
+  
+export const getServerSideProps = async () => {
+  const response = await axios.get('https://localhost:3000/api/post');
+  console.log(response.data.name);
+  return {
+    props:{}
+  }
+}
+export default Home;
